7a8665e90f085c46926354777c0717a93c974e1e
kristalys47 <kristalys.cba4@yahoo.com>
2017-03-10 09:24:20 -0400

    Practica incompleta

---
+++ PracticaParAExamen1/src/Dibujar.java
@@ -0,0 +1,18 @@
+
+
+public class Dibujar {
+	private int posx;
+	private int posy;
+	public int getposX(){return posx;}
+	public int getposY(){return posy;}
+	public void setX(int posx)
+	{
+		this.posx=posx;
+	}
+	public Dibujar(int posx, int posy)
+	{
+		
+	}
+	
+
+}
---
+++ PracticaParAExamen1/src/MyFirstAll.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class MyFirstAll {
+
+	public static void main(String[] args) {
+		JFrame hello= new JFrame();
+		hello.setVisible(true);
+		hello.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Dibujar unCirculo = new Dibujar(3, 4);
+		int x=unCirculo.getposX();
+		boolean toto;
+		if (toto==false)
+		{
+			System.out.print("yes");
+		}
+			
+		
+		
+		
+		
+	}
+
+}
+
